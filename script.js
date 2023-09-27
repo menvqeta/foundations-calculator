353102e9d7d09070e16675a0e1c232f745564eb5
menvqeta <34645476+menvqeta@users.noreply.github.com>
2023-09-27 20:22:18 +0530

    Added button event listener

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const numberButtons = document.querySelectorAll(".number-button");
-const previousResult = document.querySelector("#previous-result");
-const currentResult = document.querySelector("#current-result");
+let previousResult = document.querySelector("#previous-result");
+let currentResult = document.querySelector("#current-result");
 const clearScreenKey = document.querySelector("#clear-screen-key");
 const deleteKey = document.querySelector("#delete-key");
 const reverseSignKey = document.querySelector("#reverse-sign-key");
 const decimalKey = document.querySelector("#decimal-key");
+const percentKey = document.querySelector("#percent-key");
+const addKey = document.querySelector("#add-key");
+const subtractKey = document.querySelector("#subtract-key");
+const multiplyKey = document.querySelector("#multiply-key");
+const divideKey = document.querySelector("#divide-key");
 
 function addEventListenersToNumbers() {
     for (let i = 0; i < numberButtons.length; i++) {
@@ -35,7 +40,7 @@ function deleteDigit() {
 
 function clearScreen() {
     currentResult.textContent = 0;
-    previousResult.textContent = '';
+    previousResult.textContent = '\n';
 }
 
 function reverseSign() {
@@ -63,6 +68,40 @@ function addDecimal() {
     currentResult.textContent = currentNum;
 }
 
+function changePreviousResult(sign) {
+    if(previousResult.textContent.trim().length === 0) {
+        let prevNum = +currentResult.textContent.trim();
+        currentResult.textContent = '\n';
+        previousResult.textContent = `${prevNum} ${sign}`;
+    }
+    else {
+        let prevNum = +previousResult.textContent.slice(0, -1).trim();
+        if(currentResult.textContent.trim().length === 0) {
+            previousResult.textContent = `${prevNum} ${sign}`;
+            return;
+        }
+        let currentNum = +currentResult.textContent.trim();
+        let result = calculate(prevNum, currentNum, sign);
+        currentResult.textContent = '\n\n';
+        previousResult.textContent = `${result} ${sign}`;
+    }
+}
+
+function calculate(first, second, sign) {
+    if(sign === '+') {
+        return +first + +second;
+    }
+    else if(sign === '-') {
+        return +first - +second;
+    }
+    else if(sign === '*') {
+        return +first * +second;
+    }
+    else if(sign === '÷') {
+        return +first / +second;
+    }
+}
+
 addEventListenersToNumbers();
 
 deleteKey.addEventListener('click', () => deleteDigit());
@@ -72,4 +111,10 @@ clearScreenKey.addEventListener('click', () => clearScreen());
 reverseSignKey.addEventListener('click', () => reverseSign());
 
 decimalKey.addEventListener('click', () => addDecimal());
+
+
+addKey.addEventListener('click', () => changePreviousResult('+'));
+subtractKey.addEventListener('click', () => changePreviousResult('-'));
+multiplyKey.addEventListener('click', () => changePreviousResult('*'));
+divideKey.addEventListener('click', () => changePreviousResult('÷'));
 
